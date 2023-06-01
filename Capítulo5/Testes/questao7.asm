f14ff2171ad463a144b4527f2e755db774e956c9
analivi4 <slopeslivia@alu.ugc.br>
2023-06-01 14:59:17 -0300

    cap5

--- Capítulo5/Testes/questao7.asm
+++ Capítulo5/Testes/questao7.asm
@@ -9,20 +9,19 @@ printf  PROTO   arg1:Ptr Byte, printlist:VARARG
 scanf   PROTO   arg2:Ptr Byte, inputlist:VARARG
         .data
 msg1fmt byte    "%d",0Ah,0 
-sun     sdword ?
+sum     sdword ?
 i       sdword ?
         .code
 main    proc
-        mov i, 10
-        mov sun, 0
-        .repeat
-        mov eax, sun
-        add eax, i
-        mov sun,eax
-        mov eax, i
+        mov eax, 10
+        mov sum, 0
+repeat01: nop
+        add sum, eax
         sub eax, 2
+        cmp eax,0
+        jg repeat01
+endrpt01: nop
         mov i, eax
-        .until i <= 0
         INVOKE printf, ADDR msg1fmt, sun
         INVOKE printf, ADDR msg1fmt, i
 main    endp
